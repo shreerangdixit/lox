fe0779732648940116c908034050e28b2fd9b6d9
Shreerang Dixit <sdixit16@bloomberg.net>
2022-02-07 12:09:31 -0500

    Nit

--- interpreter/env.go
+++ interpreter/env.go
@@ -18,49 +18,49 @@ func newEnvError(msg string) EnvError {
 }
 
 type Env struct {
-	scopesVariables map[string]types.TypeValue
-	enclosing       *Env
+	scopeVariables map[string]types.TypeValue
+	enclosing      *Env
 }
 
 func NewEnv() *Env {
 	return &Env{
-		scopesVariables: make(map[string]types.TypeValue),
-		enclosing:       nil,
+		scopeVariables: make(map[string]types.TypeValue),
+		enclosing:      nil,
 	}
 }
 
 func NewEnvWithEnclosing(env *Env) *Env {
 	return &Env{
-		scopesVariables: make(map[string]types.TypeValue),
-		enclosing:       env,
+		scopeVariables: make(map[string]types.TypeValue),
+		enclosing:      env,
 	}
 }
 
 func (e *Env) Declare(varName string, varValue types.TypeValue) error {
-	if _, ok := e.scopesVariables[varName]; ok {
+	if _, ok := e.scopeVariables[varName]; ok {
 		return newEnvError(fmt.Sprintf("cannot redeclare variable %s", varName))
 	}
-	e.scopesVariables[varName] = varValue
+	e.scopeVariables[varName] = varValue
 	return nil
 }
 
 func (e *Env) Assign(varName string, varValue types.TypeValue) error {
-	if _, ok := e.scopesVariables[varName]; !ok {
+	if _, ok := e.scopeVariables[varName]; !ok {
 		if e.enclosing != nil {
 			return e.enclosing.Assign(varName, varValue)
 		}
 		return newEnvError(fmt.Sprintf("variable not declared %s", varName))
 	}
-	e.scopesVariables[varName] = varValue
+	e.scopeVariables[varName] = varValue
 	return nil
 }
 
 func (e *Env) Get(varName string) (types.TypeValue, error) {
-	if _, ok := e.scopesVariables[varName]; !ok {
+	if _, ok := e.scopeVariables[varName]; !ok {
 		if e.enclosing != nil {
 			return e.enclosing.Get(varName)
 		}
 		return types.NO_VALUE, newEnvError(fmt.Sprintf("variable not declared %s", varName))
 	}
-	return e.scopesVariables[varName], nil
+	return e.scopeVariables[varName], nil
 }
