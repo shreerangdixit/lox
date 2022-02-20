dd9e4e4a4c50405e442a1225505dab2c48bf3c95
Shreerang Dixit <sdixit16@bloomberg.net>
2022-02-20 12:00:39 -0500

    Cleanup node String() methods

--- ast/nodes.go
+++ ast/nodes.go
@@ -13,15 +13,32 @@ type Node interface {
 	String() string
 }
 
-type NilNode struct {
+type NilNode struct{}
+
+func (n NilNode) String() string {
+	return "nil"
 }
 
 type ProgramNode struct {
 	Declarations []Node
 }
 
+func (n ProgramNode) String() string {
+	str := ""
+	for _, decl := range n.Declarations {
+		str += decl.String()
+		str += "\n"
+	}
+
+	return str
+}
+
 type IdentifierNode struct {
 	Token token.Token
+}
+
+func (n IdentifierNode) String() string {
+	return n.Token.Literal
 }
 
 type AssignmentNode struct {
@@ -29,13 +46,25 @@ type AssignmentNode struct {
 	Value      Node
 }
 
+func (n AssignmentNode) String() string {
+	return fmt.Sprintf("%s=%s", n.Identifier, n.Value)
+}
+
 type VarStmtNode struct {
 	Identifier IdentifierNode
 	Value      Node
 }
 
+func (n VarStmtNode) String() string {
+	return fmt.Sprintf("var %s=%s", n.Identifier, n.Value)
+}
+
 type ExpStmtNode struct {
 	Exp Node
+}
+
+func (n ExpStmtNode) String() string {
+	return n.Exp.String()
 }
 
 type IfStmtNode struct {
@@ -44,25 +73,56 @@ type IfStmtNode struct {
 	FalseStmt Node
 }
 
+func (n IfStmtNode) String() string {
+	if n.FalseStmt != nil {
+		return fmt.Sprintf("if(%s)\n\t%s\nelse\t%s", n.Exp, n.TrueStmt, n.FalseStmt)
+	} else {
+		return fmt.Sprintf("if(%s)\n\t%s", n.Exp, n.TrueStmt)
+	}
+}
+
 type WhileStmtNode struct {
 	Condition Node
 	Body      Node
+}
+
+func (n WhileStmtNode) String() string {
+	return fmt.Sprintf("while(%s)\n\t%s", n.Condition, n.Body)
 }
 
 type BreakStmtNode struct {
 	Token token.Token
 }
 
+func (n BreakStmtNode) String() string {
+	return n.Token.String()
+}
+
 type ReturnStmtNode struct {
 	Exp Node
+}
+
+func (n ReturnStmtNode) String() string {
+	if n.Exp != nil {
+		return n.Exp.String()
+	}
+	return "return"
 }
 
 type BlockNode struct {
 	Declarations []Node
 }
 
+func (n BlockNode) String() string {
+	return fmt.Sprintf("{\n\t%s\n}", n.Declarations)
+}
+
 type ExpNode struct {
 	Exp Node
+}
+
+func (n ExpNode) String() string {
+	return n.Exp.String()
 }
 
 type TernaryOpNode struct {
@@ -71,10 +131,18 @@ type TernaryOpNode struct {
 	FalseExp Node
 }
 
+func (n TernaryOpNode) String() string {
+	return fmt.Sprintf("%s ? %s : %s", n.Exp, n.TrueExp, n.FalseExp)
+}
+
 type BinaryOpNode struct {
 	LeftExp  Node
 	Op       token.Token
 	RightExp Node
+}
+
+func (n BinaryOpNode) String() string {
+	return fmt.Sprintf("%s %s %s", n.LeftExp, n.Op, n.RightExp)
 }
 
 type UnaryOpNode struct {
@@ -82,9 +150,17 @@ type UnaryOpNode struct {
 	Operand Node
 }
 
+func (n UnaryOpNode) String() string {
+	return fmt.Sprintf("%s%s", n.Op, n.Operand)
+}
+
 type LogicalAndNode struct {
 	LHS Node
 	RHS Node
+}
+
+func (n LogicalAndNode) String() string {
+	return fmt.Sprintf("%s && %s", n.LHS, n.RHS)
 }
 
 type LogicalOrNode struct {
@@ -92,20 +168,45 @@ type LogicalOrNode struct {
 	RHS Node
 }
 
+func (n LogicalOrNode) String() string {
+	return fmt.Sprintf("%s || %s", n.LHS, n.RHS)
+}
+
 type BooleanNode struct {
 	Token token.Token
+}
+
+func (n BooleanNode) String() string {
+	return n.Token.String()
 }
 
 type NumberNode struct {
 	Token token.Token
 }
 
+func (n NumberNode) String() string {
+	return n.Token.Literal
+}
+
 type StringNode struct {
 	Token token.Token
 }
 
+func (n StringNode) String() string {
+	return n.Token.Literal
+}
+
 type ListNode struct {
 	Elements []Node
+}
+
+func (n ListNode) String() string {
+	str := ""
+	for _, elem := range n.Elements {
+		str += elem.String()
+		str += " "
+	}
+	return str
 }
 
 type CallNode struct {
@@ -113,9 +214,17 @@ type CallNode struct {
 	Arguments []Node
 }
 
+func (n CallNode) String() string {
+	return fmt.Sprintf("fun %s(%s)", n.Callee, n.Arguments)
+}
+
 type IndexOfNode struct {
 	Sequence Node
 	Index    Node
+}
+
+func (n IndexOfNode) String() string {
+	return fmt.Sprintf("%s[%s]", n.Sequence, n.Index)
 }
 
 type FunctionNode struct {
@@ -124,42 +233,6 @@ type FunctionNode struct {
 	Body       BlockNode
 }
 
-func (n NilNode) String() string        { return "nil" }
-func (n ProgramNode) String() string    { return fmt.Sprintf("%s", n.Declarations) }
-func (n IdentifierNode) String() string { return n.Token.String() }
-func (n AssignmentNode) String() string { return fmt.Sprintf("%s=%s", n.Identifier, n.Value) }
-func (n VarStmtNode) String() string    { return fmt.Sprintf("var %s=%s", n.Identifier, n.Value) }
-func (n BlockNode) String() string      { return fmt.Sprintf("{%s}", n.Declarations) }
-func (n ExpStmtNode) String() string    { return n.Exp.String() }
-func (n IfStmtNode) String() string {
-	if n.FalseStmt != nil {
-		return fmt.Sprintf("if(%s) %s else %s", n.Exp, n.TrueStmt, n.FalseStmt)
-	} else {
-		return fmt.Sprintf("if(%s) %s", n.Exp, n.TrueStmt)
-	}
-}
-func (n WhileStmtNode) String() string { return fmt.Sprintf("while(%s)%s", n.Condition, n.Body) }
-func (n BreakStmtNode) String() string { return n.Token.String() }
-func (n ReturnStmtNode) String() string {
-	if n.Exp != nil {
-		return n.Exp.String()
-	}
-	return "return"
-}
-func (n ExpNode) String() string { return n.Exp.String() }
-func (n TernaryOpNode) String() string {
-	return fmt.Sprintf("%s ? %s : %s", n.Exp, n.TrueExp, n.FalseExp)
-}
-func (n LogicalAndNode) String() string { return fmt.Sprintf("%s && %s", n.LHS, n.RHS) }
-func (n LogicalOrNode) String() string  { return fmt.Sprintf("%s || %s", n.LHS, n.RHS) }
-func (n BinaryOpNode) String() string   { return fmt.Sprintf("%s %s %s", n.LeftExp, n.Op, n.RightExp) }
-func (n UnaryOpNode) String() string    { return fmt.Sprintf("%s%s", n.Op, n.Operand) }
-func (n BooleanNode) String() string    { return n.Token.String() }
-func (n NumberNode) String() string     { return n.Token.String() }
-func (n StringNode) String() string     { return n.Token.String() }
-func (n CallNode) String() string       { return fmt.Sprintf("func %s(%s)", n.Callee, n.Arguments) }
-func (n ListNode) String() string       { return fmt.Sprintf("%s", n.Elements) }
-func (n IndexOfNode) String() string    { return fmt.Sprintf("%s[%s]", n.Sequence, n.Index) }
 func (n FunctionNode) String() string {
-	return fmt.Sprintf("fun %s(%s)%s", n.Identifier, n.Parameters, n.Body)
+	return fmt.Sprintf("fun %s(%s)\n%s", n.Identifier, n.Parameters, n.Body)
 }
