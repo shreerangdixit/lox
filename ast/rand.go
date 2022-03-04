bb392b37fd01be2dfc2042df3740aadaca92f3ae
Shreerang Dixit <sdixit16@bloomberg.net>
2022-03-03 23:27:34 -0500

    Fix randomizer

--- ast/rand.go
+++ ast/rand.go
@@ -2,14 +2,18 @@ package ast
 
 import (
 	"math/rand"
+	"time"
 )
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 func RandStringBytes(n int) string {
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+
 	b := make([]byte, n)
 	for i := range b {
-		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+		b[i] = letterBytes[r.Intn(len(letterBytes))]
 	}
 	return string(b)
 }
