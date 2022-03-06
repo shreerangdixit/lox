cd410ef105951744d74ff4e96e3d57c60e6843f2
Shreerang Dixit <sdixit16@bloomberg.net>
2022-03-06 15:36:53 -0500

    Fix imports

--- run/file.go
+++ run/file.go
@@ -1,9 +1,23 @@
 package run
 
 import (
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/shreerangdixit/redes/eval"
 )
 
 func RunFile(file string) error {
-	return eval.Import(eval.NewEvaluator(), eval.ModuleFromFile(file))
+	abspath, err := filepath.Abs(file)
+	if err != nil {
+		panic(err)
+	}
+
+	basedir := strings.TrimSuffix(abspath, filepath.Base(abspath))
+	if err := os.Chdir(basedir); err != nil {
+		panic(err)
+	}
+
+	return eval.Import(eval.NewEvaluator(), eval.ModuleFromFile(abspath))
 }
